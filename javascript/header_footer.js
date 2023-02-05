9d0320e72913eff11291ef47d7fcc624a3b23083
HSUCHIAHUNG <h0989541162@gmail.com>
2023-02-05 15:59:02 +0800

    202302051558

--- javascript/header_footer.js
+++ javascript/header_footer.js
@@ -2,10 +2,14 @@
 let menu = document.querySelector("#menu_bar");
 let navbar = document.querySelector(".nav_bar");
 let nav_icons = document.querySelector(".nav_icons")
+let header_main = document.querySelector(".header_main")
 
 //按下按鈕產生的變化
 menu.addEventListener("click",function(){
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
     nav_icons.classList.toggle('active');
+    header_main.classList.toggle('activeh');
+    navbar.classList.toggle('activeh');
 }); 
+
